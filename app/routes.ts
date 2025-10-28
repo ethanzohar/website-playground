73059b1aa913b6548d065b05fece011aba5c504c
ethanzohar <ethanzohar9@gmail.com>
2025-10-27 21:24:27 -0400

    ascii art

--- app/routes.ts
+++ app/routes.ts
@@ -6,5 +6,6 @@ export default [
   route("mii_plaza", "routes/mii_plaza.tsx"),
   route("mii_loader", "routes/mii_loader.tsx"),
   route("mii_builder", "routes/mii_builder.tsx"),
-  route("mii_plaza_2", "routes/mii_plaza_2.tsx")
+  route("mii_plaza_2", "routes/mii_plaza_2.tsx"),
+  route("image_explode", "routes/image_explode.tsx")
 ] satisfies RouteConfig;
